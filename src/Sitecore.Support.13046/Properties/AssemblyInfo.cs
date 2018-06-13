3bb95842ea50a3fc04a7fb1079a8b6e44e7078f9
DK\IRO <iro@sitecore.net>
2018-06-13 13:31:08 +0300

    Map controller is centralized on a central point if the location finder is empty

---
+++ src/Sitecore.Support.13046/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13046")]
+[assembly: AssemblyProduct("Sitecore.Support.13046")]
+[assembly: ComVisible(false)]
